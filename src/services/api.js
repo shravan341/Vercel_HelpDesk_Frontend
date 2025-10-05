344571c7ac2f96530363292b37345e20b89c78f5
shravan341 <shravankumar.21a@gmail.com>
2025-10-05 09:42:27 +0530

    3rd

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
+const API_BASE =
+  import.meta.env.VITE_API_BASE ||
+  "https://vercel-help-desk-backend.vercel.app/api";
 
 const instance = axios.create({
   baseURL: API_BASE,
 });
 
 export const setAuthToken = (token) => {
-  if (token) instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  if (token)
+    instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   else delete instance.defaults.headers.common["Authorization"];
 };
 
